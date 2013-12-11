da2319f4e4f178d58cea9c676a245d37ebc839d7
Kyle Morse <kmorse@rallydev.com>
2013-12-10 21:16:28 -0700

    Super basic crud done

--- examples/crud.js
+++ examples/crud.js
@@ -1,40 +1,46 @@
 var rally = require('./../lib/main.js'),
-  restApi = new rally.RestApi();
+  restApi = new rally.RestApi({server: 'https://test7cluster.rallydev.com'});
 
 function createDefect() {
     return restApi.create({
         type: 'defect',
         data: {
-            Name: 'My Defect'
+            Name: 'My Defect',
+            Environment: 'Test'
         }
         //todo: request options, rally options (fetch, etc)
     });
 }
 
 function readDefect(result) {
-    return restApi.get();
+    return restApi.get({
+        ref: '/defect/' + result.Object.ObjectID //todo: use ref instead
+    });
 }
 
-//function updateDefect(error, result) {
-//
-//}
-//
-//function deleteDefect(error, result) {
-//
-//}
-//
-//function done() {
-//
-//}
-//
+function updateDefect(result) {
+    //todo: inconsistent result from read (should be stored on Object property?
+    return restApi.update({
+        ref: '/defect/' + result.ObjectID, //todo: use ref instead
+        data: {
+            Name: 'My Updated Defect'
+        }
+    });
+}
+
+function deleteDefect(result) {
+    return restApi.delete({
+        ref: '/defect/' + result.Object.ObjectID  //todo: use ref instead
+    });
+}
+
 createDefect()
     .then(readDefect)
+    .then(updateDefect)
+    .then(deleteDefect)
     .fail(function(errors) {
         console.log(errors);
     });
-//    .then(updateDefect)
-//    .then(deleteDefect)
-//    .then()
 
 //var promise = restApi.get({
 //    ref: '/hierarchicalrequirement/15176552210'
