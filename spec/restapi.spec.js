3d1273f570711051f6e70b1e12e6225aa4e7b050
Kyle Morse <kmorse@rallydev.com>
2013-12-16 08:34:01 -0700

    all rest api tests except query

--- spec/restapi.spec.js
+++ spec/restapi.spec.js
@@ -1,15 +1,16 @@
 var should = require('should'),
     RestApi = require('../lib/restapi'),
     request = require('../lib/request'),
-    sinon = require('sinon');
+    sinon = require('sinon'),
+    Q = require('q');
 
 describe('RestApi', function() {
 
     beforeEach(function() {
-        this.get = sinon.stub(request.Request.prototype, 'get');
-        this.put = sinon.stub(request.Request.prototype, 'put');
-        this.post = sinon.stub(request.Request.prototype, 'post');
-        this.delete = sinon.stub(request.Request.prototype, 'delete');
+        this.get = sinon.stub(request.Request.prototype, 'get').returns(Q());
+        this.put = sinon.stub(request.Request.prototype, 'put').returns(Q());
+        this.post = sinon.stub(request.Request.prototype, 'post').returns(Q());
+        this.delete = sinon.stub(request.Request.prototype, 'delete').returns(Q());
     });
 
     afterEach(function() {
@@ -90,21 +91,211 @@ describe('RestApi', function() {
 
     describe('#create', function() {
 
+        it('translates request options', function() {
+            var restApi = new RestApi();
+            restApi.create({
+                type: 'defect',
+                data: {
+                    Name: 'A defect'
+                },
+                fetch: ['FormattedID'],
+                scope: {workspace: '/workspace/1234'},
+                requestOptions: {
+                    qs: {foo: 'bar'}
+                }
+            });
+
+            this.post.callCount.should.eql(1);
+            var args = this.post.firstCall.args[0];
+            args.qs.fetch.should.eql('FormattedID');
+            args.qs.workspace.should.eql('/workspace/1234');
+            args.qs.foo.should.eql('bar');
+        });
+
+        it('generates correct post request', function() {
+            var restApi = new RestApi();
+            var callback = sinon.stub();
+            var promise = restApi.create({
+                type: 'defect',
+                data: {
+                    Name: 'A defect'
+                }
+            }, callback);
+
+            this.post.callCount.should.eql(1);
+            var args = this.post.firstCall.args;
+            args[0].url.should.eql('/defect/create');
+            args[0].json.should.eql({defect: {Name: 'A defect'}});
+            args[1].should.be.exactly(callback);
+            this.post.firstCall.returnValue.should.be.exactly(promise);
+        });
     });
 
     describe('#update', function() {
 
-    });
+        it('translates request options', function() {
+            var restApi = new RestApi();
+            restApi.update({
+                ref: '/defect/1234',
+                data: {
+                    Name: 'Updated defect'
+                },
+                fetch: ['FormattedID'],
+                scope: {workspace: '/workspace/1234'},
+                requestOptions: {
+                    qs: {foo: 'bar'}
+                }
+            });
 
-    describe('#get', function() {
+            this.put.callCount.should.eql(1);
+            var args = this.put.firstCall.args[0];
+            args.qs.fetch.should.eql('FormattedID');
+            args.qs.workspace.should.eql('/workspace/1234');
+            args.qs.foo.should.eql('bar');
+        });
 
+        it('generates correct put request', function() {
+            var restApi = new RestApi();
+            var callback = sinon.stub();
+            var promise = restApi.update({
+                ref: {_ref: '/defect/1234'},
+                data: {
+                    Name: 'Updated defect'
+                }
+            }, callback);
+
+            this.put.callCount.should.eql(1);
+            var args = this.put.firstCall.args;
+            args[0].url.should.eql('/defect/1234');
+            args[0].json.should.eql({defect: {Name: 'Updated defect'}});
+            args[1].should.be.exactly(callback);
+            this.put.firstCall.returnValue.should.be.exactly(promise);
+        });
     });
 
     describe('#delete', function() {
 
+        it('translates request options', function() {
+            var restApi = new RestApi();
+            restApi.delete({
+                ref: '/defect/1234',
+                scope: {workspace: '/workspace/1234'},
+                requestOptions: {
+                    qs: {foo: 'bar'}
+                }
+            });
+
+            this.delete.callCount.should.eql(1);
+            var args = this.delete.firstCall.args[0];
+            args.qs.workspace.should.eql('/workspace/1234');
+            args.qs.foo.should.eql('bar');
+        });
+
+        it('generates correct del request', function() {
+            var restApi = new RestApi();
+            var callback = sinon.stub();
+            var promise = restApi.delete({
+                ref: {_ref: '/defect/1234'}
+            }, callback);
+
+            this.delete.callCount.should.eql(1);
+            var args = this.delete.firstCall.args;
+            args[0].url.should.eql('/defect/1234');
+            args[1].should.be.exactly(callback);
+            this.delete.firstCall.returnValue.should.be.exactly(promise);
+        });
+    });
+
+    describe('#get', function() {
+
+        it('translates request options', function() {
+            var restApi = new RestApi();
+            restApi.get({
+                ref: '/defect/1234',
+                scope: {workspace: '/workspace/1234'},
+                fetch: ['FormattedID'],
+                requestOptions: {
+                    qs: {foo: 'bar'}
+                }
+            });
+
+            this.get.callCount.should.eql(1);
+            var args = this.get.firstCall.args[0];
+            args.qs.workspace.should.eql('/workspace/1234');
+            args.qs.fetch.should.eql('FormattedID');
+            args.qs.foo.should.eql('bar');
+        });
+
+        it('generates correct get request', function() {
+            var restApi = new RestApi();
+            var callback = sinon.stub();
+            restApi.get({
+                ref: {_ref: '/defect/1234'}
+            }, callback);
+
+            this.get.callCount.should.eql(1);
+            var args = this.get.firstCall.args;
+            args[0].url.should.eql('/defect/1234');
+        });
+
+        it('calls back with transformed result', function(done) {
+            this.get.yieldsAsync(null, {Errors: [], Warnings: [], Name: 'Foo'});
+            var restApi = new RestApi();
+            restApi.get({
+                ref: {_ref: '/defect/1234'}
+            }, function(error, result) {
+                should.not.exist(error);
+                result.Errors.should.eql([]);
+                result.Warnings.should.eql([]);
+                result.Object.should.eql({Name: 'Foo'});
+                done();
+            });
+        });
+
+        it('resolves promise with transformed result', function(done) {
+            this.get.yieldsAsync(null, {Errors: [], Warnings: [], Name: 'Foo'});
+            var restApi = new RestApi();
+            var onError = sinon.stub();
+            restApi.get({
+                ref: {_ref: '/defect/1234'}
+            }).then(function(result) {
+                    onError.callCount.should.eql(0);
+                    result.Errors.should.eql([]);
+                    result.Warnings.should.eql([]);
+                    result.Object.should.eql({Name: 'Foo'});
+                    done();
+                }, onError);
+        });
+
+        it('calls back with error', function(done) {
+            var error = 'Error!';
+            this.get.yieldsAsync([error], null);
+            var restApi = new RestApi();
+            restApi.get({
+                ref: {_ref: '/defect/1234'}
+            }, function(err, result) {
+                err.should.eql([error]);
+                should.not.exist(result);
+                done();
+            });
+        });
+
+        it('rejects promise with error', function(done) {
+            var error = 'Error!';
+            this.get.yieldsAsync([error], null);
+            var restApi = new RestApi();
+            var onSuccess = sinon.stub();
+            restApi.get({
+                ref: {_ref: '/defect/1234'}
+            }).then(onSuccess, function(err) {
+                    onSuccess.callCount.should.eql(0);
+                    err.should.eql([error]);
+                    done();
+                });
+        });
     });
 
     describe('#query', function() {
-
+        //todo
     });
 });
